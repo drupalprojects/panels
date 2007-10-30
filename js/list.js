c6cd6ff25c55e6d6f7b7e2671823b57a57657803
Earl Miles <merlin@logrus.com>
2007-10-30 19:24:48 +0000

    set panels weight to 10; fix problem with js buttons if using tables in theme

--- js/list.js
+++ js/list.js
@@ -19,7 +19,7 @@
 Drupal.list = function(base, settings) {
   // Set the properties for this object.
   if (settings.container == null) {
-    settings.container = 'tr';
+    settings.container = settings.row_class;
   }
 
   var max = function(array) {
