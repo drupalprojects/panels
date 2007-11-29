86310d1e7f534ea0501f3384d4e55ca0198038eb
Earl Miles <merlin@logrus.com>
2007-11-29 16:42:43 +0000

    add missing js file

---
+++ js/checkboxes.js
@@ -0,0 +1,38 @@
+// $Id$
+
+if (!Drupal.Panels) {
+  Drupal.Panels = {};
+}
+
+Drupal.Panels.Checkboxes = {};
+
+/** Bind an item to a checkbox to auto disable it when unchecked **/
+Drupal.Panels.Checkboxes.bindCheckbox = function(checkbox, gadget) {
+  console.log(checkbox);
+  var clickCheckBox = function() {
+    var status = !$(checkbox).attr('checked');
+
+    for (var i in gadget) {
+      $(gadget[i]).attr('disabled', status);
+    }
+  }
+
+  $(checkbox).change(); // unset any existing
+  $(checkbox).change(clickCheckBox);
+  clickCheckBox();
+}
+
+
+Drupal.Panels.Checkboxes.bindCheckboxes = function() {
+  if (Drupal.settings && Drupal.settings.panels && Drupal.settings.panels.checkboxes) {
+    for (var checkbox in Drupal.settings.panels.checkboxes) {
+      if (!$(checkbox + '.checkbox-processed').size()) {
+        Drupal.Panels.Checkboxes.bindCheckbox(checkbox, Drupal.settings.panels.checkboxes[checkbox]);
+        $(checkbox).addClass('checkbox-processed');
+      }
+    }
+  }
+
+}
+
+$(Drupal.Panels.Checkboxes.bindCheckboxes);
