03a83f7759a085cff9a3ccf02b5678f71334ab6b
Earl Miles <merlin@logrus.com>
2008-01-08 18:40:14 +0000

    #206882: Alleviate (but not fix, sadly) hover problems with panels admin links on a panel display

--- js/panels.js
+++ js/panels.js
@@ -6,9 +6,11 @@ Drupal.Panels.autoAttach = function() {
   $("div.panel-pane").hover(
     function() { 
       $('div.panel-hide', this).addClass("hover"); 
+      return true;
     }, 
     function(){ 
       $('div.panel-hide', this).removeClass("hover"); 
+      return true;
     }
   );
 }
