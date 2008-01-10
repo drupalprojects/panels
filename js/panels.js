40e0cfe9c926507dce75801a8be28e266ec86d5e
Earl Miles <merlin@logrus.com>
2008-01-10 17:38:34 +0000

    #206882: Fix js/css to reduce interference with other modules such as fivestar

--- js/panels.js
+++ js/panels.js
@@ -6,12 +6,12 @@ Drupal.Panels.autoAttach = function() {
   // Using .hover seems to mess with the href in statusbar when hovering over links in FF.
   $("div.panel-pane").mouseover(
     function() {
-      $('div.panel-hide', this).addClass("hover"); return true;
+      $('div.panel-hide', this).addClass("panel-hide-hover"); return true;
     }
    );
   $("div.panel-pane").mouseout(
    function(){
-      $('div.panel-hide', this).removeClass("hover"); return true;
+      $('div.panel-hide', this).removeClass("panel-hide-hover"); return true;
     }
   );
 }
