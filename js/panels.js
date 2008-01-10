a5b0662b3f1aa1d38971a377c7fd9cd302dcf153
Earl Miles <merlin@logrus.com>
2008-01-10 17:51:57 +0000

    #206882: Rely on CSS hover for browsers other than IE.

--- js/panels.js
+++ js/panels.js
@@ -3,14 +3,17 @@
 Drupal.Panels = {};
 
 Drupal.Panels.autoAttach = function() {
-  $("div.panel-pane").hover(
-    function() {
-      $('div.panel-hide', this).addClass("panel-hide-hover"); return true;
-    },
-    function(){
-      $('div.panel-hide', this).removeClass("panel-hide-hover"); return true;
-    }
-  );
+  if ($.browser.msie) {
+    // If IE, attach a hover event so we can see our admin links.
+    $("div.panel-pane").hover(
+      function() {
+        $('div.panel-hide', this).addClass("panel-hide-hover"); return true;
+      },
+      function(){
+        $('div.panel-hide', this).removeClass("panel-hide-hover"); return true;
+      }
+    );
+  }
 }
 
 $(Drupal.Panels.autoAttach);
