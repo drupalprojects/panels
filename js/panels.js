182dd9f5ecf69df6a74583844e4b35402c5a7f6f
Earl Miles <merlin@logrus.com>
2008-01-10 17:44:10 +0000

    Revert panels.js to using hover

--- js/panels.js
+++ js/panels.js
@@ -3,14 +3,11 @@
 Drupal.Panels = {};
 
 Drupal.Panels.autoAttach = function() {
-  // Using .hover seems to mess with the href in statusbar when hovering over links in FF.
-  $("div.panel-pane").mouseover(
+  $("div.panel-pane").hover(
     function() {
       $('div.panel-hide', this).addClass("panel-hide-hover"); return true;
-    }
-   );
-  $("div.panel-pane").mouseout(
-   function(){
+    },
+    function(){
       $('div.panel-hide', this).removeClass("panel-hide-hover"); return true;
     }
   );
