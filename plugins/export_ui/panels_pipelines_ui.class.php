2e728344842c2d53073306ddfe6040d8432d30a4
Sam Boyer <drupal@samboyer.org>
2011-11-27 04:09:44 -0500

    Incremental checkin; add some basic operations (though not NEARLY the whole set).

--- plugins/export_ui/panels_pipelines_ui.class.php
+++ plugins/export_ui/panels_pipelines_ui.class.php
@@ -14,22 +14,52 @@ class panels_pipelines_ui extends ctools_export_ui {
       return;
     }
 
+    dsm($this, 'pipelines ui object');
+
     // Make the list page appear as a tab under the general Panels settings.
     $this->plugin['menu']['items']['list callback']['type'] = MENU_LOCAL_TASK;
-
-    // Pattern for internal callbacks is pulled from page manager.
-    $items['admin/structure/panels/pipelines/%ctools_js/operation/%panels_pipeline_cache'] = array(
-      'page callback' => 'panels_edit_pipeline_operation',
-      'page arguments' => array(4, 6),
-      'access arguments' => array('administer panels pipelines'),
-      'theme callback' => 'ajax_base_page_theme',
-      'type' => MENU_CALLBACK,
-    );
 
     parent::hook_menu($items);
   }
 
-  public function list_build_row($item, &$form_state, $operations) {
-    parent::list_build_row($item, $form_state, $operations);
+  public function get_default_operation_trail($item, $operations) {
+    return array('operations', 'summary');
+  }
+
+  public function get_operations($item) {
+    $operations = array();
+
+    // The primary "operations" group is anonymous.
+    $operations['operations'] = array(
+      '#type' => 'ctools_operation_group',
+    );
+
+    $operations['operations']['summary'] = array(
+      '#type' => 'link',
+      '#title' => t('Summary'),
+      '#description' => t('See a summary of all the variants contained within this pipeline.'),
+    );
+
+    $operations['operations']['edit'] = array(
+      '#type' => 'link',
+      '#title' => t('Edit'),
+      '#description' => t('Edit this item'),
+      '#operation' => array(
+        'form' => 'ctools_export_ui_edit_item_form',
+      ),
+    );
+
+    $operations['actions'] = array(
+      '#type' => 'ctools_operation_group',
+    );
+
+    $operations['actions']['disable'] = array(
+      '#type' => 'link',
+      '#title' => t('Disable'),
+      '#description' => t('Disable this pipeline'),
+    );
+
+    drupal_alter('ctools_export_ui_operations', $operations, $this);
+    return $operations;
   }
 }
