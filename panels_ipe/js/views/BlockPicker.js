4b2874b9800b8926d93765bd6e5f4f88dd620114
Samuel Mortenson <samuel.mortenson@acquia.com>
2016-03-13 21:12:37 -0700

    Use a flex-based display for the "Place Content" tab in Panels IPE.

--- panels_ipe/js/views/BlockPicker.js
+++ panels_ipe/js/views/BlockPicker.js
@@ -120,6 +120,9 @@
       // Render our categories.
       this.renderCategories();
 
+      // Add a unique class to our top region to scope CSS.
+      this.$('.ipe-category-picker-top').addClass('ipe-block-picker-list');
+
       // If we're viewing the current layout tab, show a custom item.
       var on_screen_count = 0;
       Drupal.panels_ipe.app.get('layout').get('regionCollection').each(function (region) {
