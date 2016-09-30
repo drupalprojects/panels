607d2d18017085beb90694755bf730032295b43d
eclipsegc <eclipsegc@61203.no-reply.drupal.org>
2016-09-30 16:34:40 +0100

    Issue #2667754 by dsnopek, EclipseGc, japerry, phenaproxima: Allow other modules to disable the IPE based on custom logic

--- panels_ipe/src/Plugin/DisplayBuilder/InPlaceEditorDisplayBuilder.php
+++ panels_ipe/src/Plugin/DisplayBuilder/InPlaceEditorDisplayBuilder.php
@@ -157,6 +157,9 @@ class InPlaceEditorDisplayBuilder extends StandardDisplayBuilder {
   public function build(PanelsDisplayVariant $panels_display) {
     // Check to see if the current user has permissions to use the IPE.
     $has_permission = $this->account->hasPermission('access panels in-place editing') && $this->panelsStorage->access($panels_display->getStorageType(), $panels_display->getStorageId(), 'update', $this->account)->isAllowed();
+    if ($has_permission) {
+      $has_permission = \Drupal::service('plugin.manager.ipe_access')->access($panels_display);
+    }
 
     // Attach the Panels In-place editor library based on permissions.
     if ($has_permission) {
